d81d18c9ac33da6f6175128c0f48e2a043a73c65
eardali <>
2023-08-07 17:43:26 +0300

    add option to parse files collected without -P flag

--- parse-tput/parse_tput.py
+++ parse-tput/parse_tput.py
@@ -8,6 +8,7 @@
 #update iperf time interval per requirement, i variable
 #creates a plot, time vs tput
 #emrea, July 2021
+#add option to parse files collected without -P flag, Aug 2023
 
 import os
 import re
@@ -20,11 +21,17 @@ i = 1 #-i arg of iperf when getting result txt file
 #tput_regex = "\[SUM\].+(?:(?:M|G)Bytes)+\s+(\d+.\d+).+(?:(?:M|G)bits\/sec)"
 #tput_regex = "\[SUM\].+(?:(?:M|G)Bytes)+\s+(\d+.\d+).+(?:(?:M|G)bits\/sec\s+$)" 
 tput_regex = r"\[SUM\].+(?:(?:M|G|)Bytes)+\s+(\d+.\d+).+(?:(?:M|G|)bits\/sec\s+(?:\d+|)\s+$)"
+#when -P flag is not used (single stream)
+tput_regex_noP = r"\[.+\].+(?:(?:M|G|)Bytes)+\s+(\d+.\d+).+(?:(?:M|G|)bits\/sec\s+(?:\d+|)\s+$)"
+#tput_regex = r"\[SUM\].+(?:(?:M|G|)Bytes)+\s+(\d+.\d+).+(?:(?:M|G|)bits\/sec\s+(?:\d+|))"
 #use "$" to stop before any other string, hence avoid to catch result summary lines at the end
 #update to catch in DL direction in which result followed by retry column (i think this happen in Linux versions of iperf3)
 
-unit_regex = r"\[SUM\].+(?:(?:M|G|)Bytes)+\s+\d+.\d+.+(M|K)bits\/sec\s+(?:\d+|)\s+$"
 #this is to catch if unit is bits/sec, or Kbits/sec or Mbits/sec, normally it should be Mbits/sec, however rarely it may occurs in other way, in this case calculate Mbits/sec
+unit_regex = r"\[SUM\].+(?:(?:M|G|)Bytes)+\s+\d+.\d+.+(M|K)bits\/sec\s+(?:\d+|)\s+$"
+#when -P flag is not used (single stream)
+unit_regex_noP = r"\[.+\].+(?:(?:M|G|)Bytes)+\s+\d+.\d+.+(M|K)bits\/sec\s+(?:\d+|)\s+$"
+
 
 infile_ext = ".txt" #iperf results txt
 outfile_ext = ".csv"
@@ -33,19 +40,16 @@ figfile_ext = ".png"
 dirPath = os.path.dirname(os.path.realpath(__file__))
 
 
-for fileName in os.listdir(dirPath):   
+def parseIperfOutput(file, tput_re, unit_re):
     hit = 0 #total regex hit
     K_hit = 0 #hit for non-M, Kbits/sec hits
     b_hit = 0 #hit for non-M, bits/sec hits
-    if fileName.endswith(infile_ext):
-        print("Parsing file: %s" % fileName)
-        tput_results=[]
-        file = open(os.path.join(dirPath,fileName), 'r')
-        for line in file:
-            result = re.findall(tput_regex, line)
+    tput_results = []
+    for line in file:
+            result = re.findall(tput_re, line)
             if result:
                 hit = hit+1
-                unit = re.findall(unit_regex, line)
+                unit = re.findall(unit_re, line)
                 if unit:
                     if unit[0] == "M": #Mbits/sec, expected unit
                         tput_results.append(result[0].replace(".", ",")) #replace dot with coma, for excel calculations
@@ -57,11 +61,29 @@ for fileName in os.listdir(dirPath):
                      b_hit = b_hit+1
                      value = float(result[0])/(1024*1024)
                      tput_results.append(str(value).replace(".", ","))
-        
-        file.close()
-        print("Number of total hits: %s, Kbits/sec hits: %s, bits/sec hits: %s" %(hit, K_hit, b_hit))
-        
-        if not (hit==0 and K_hit==0 and b_hit==0):    
+
+    return tput_results, hit, K_hit, b_hit
+
+
+if __name__ == "__main__":
+    for fileName in os.listdir(dirPath): 
+        tput_results = []
+        hit = 0 #total regex hit
+        K_hit = 0 #hit for non-M, Kbits/sec hits
+        b_hit = 0 #hit for non-M, bits/sec hits
+        if fileName.endswith(infile_ext):
+            print("Parsing file: %s" % fileName)
+            file = open(os.path.join(dirPath,fileName), 'r')
+            tput_results, hit, K_hit, b_hit = parseIperfOutput(file, tput_regex, unit_regex)
+            
+            if (hit==0 and K_hit==0 and b_hit==0):
+                print("Seems no hit in %s, probably collected without -P flag, researching..." % fileName)
+                file.seek(0) #go to begging of file, since already reach EOF
+                tput_results, hit, K_hit, b_hit = parseIperfOutput(file, tput_regex_noP, unit_regex_noP)
+            
+            print("Number of total hits: %s, Kbits/sec hits: %s, bits/sec hits: %s\n" %(hit, K_hit, b_hit))
+            file.close()
+            
             #write results to csv file
             outfname = fileName.replace(infile_ext, outfile_ext)
             file = open(os.path.join(dirPath,outfname), 'w')        
@@ -105,6 +127,4 @@ for fileName in os.listdir(dirPath):
             
             fig.clf()
             plt.close()
-            
-        else:
-             print("Seems no hit in %s, may be collected without -P flag?" % fileName)
+                
